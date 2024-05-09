7a909fd05767631db2148e6027500340ad4c2a16
arielherself <ariel@arielherself.xyz>
2024-05-09 17:40:01 +0800

    feat(db): functioning init.sql file

--- db.sql
+++ db.sql
@@ -1,10 +1,14 @@
 CREATE TABLE auth_user (
     id serial PRIMARY KEY,
-    username character varying(255) NOT NULL UNIQUE,
+    name character varying(255) NOT NULL,
     password character varying(255) NOT NULL,
-    is_superuser boolean NOT NULL,
-    first_name character varying(255) NOT NULL,
-    last_name character varying(255) NOT NULL,
-    email character varying(255) NOT NULL,
-    is_active boolean NOT NULL
+    email character varying(255) NOT NULL UNIQUE
 );
+
+CREATE TABLE restaurants (
+    id serial PRIMARY KEY,
+    user_id integer references auth_user,
+    name character varying(255) NOT NULL,
+    address character varying(255) NOT NULL,
+    contact character varying(255) NOT NULL
+)
